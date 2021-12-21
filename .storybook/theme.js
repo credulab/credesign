611d0da68e7550609e1998ed22ba9036f7e741e0
Ayrton Souza <ayrtonsouza@icloud.com>
2021-12-21 00:22:32 -0300

    [:tada:] initial commit

---
+++ .storybook/theme.js
@@ -0,0 +1,21 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+
+  colorSecondary: '#6CF8F8',
+
+  appBg: '#181C23',
+  appContentBg: '#1F2029',
+  appBorderColor: '#6CF8F8',
+
+  textColor: '#EEEEF2',
+
+  barTextColor: '#E6E6E6',
+  barSelectedColor: '#D1D2DC',
+  barBg: '#181B23',
+
+  brandTitle: 'Credesign',
+  brandUrl: 'https://credu.com.br/',
+  brandImage: 'https://user-images.githubusercontent.com/30063455/146852734-93b1846c-e4ad-415d-a69f-08381c978c4b.png'
+});
